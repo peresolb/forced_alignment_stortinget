53a79485f67ad56100709483d27e9b4ceec0ca51
Per Erik Solberg <per.solberg@nb.no>
2019-11-20 09:28:52 +0100

    small adjustments to the pipeline script

--- alignment_pipeline.sh
+++ alignment_pipeline.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 if test $# -ne 2
 then
-echo "Usage:$0 <date> <id>"
-exit
+    echo "Usage:$0 <date> <id>"
+    exit
 fi
 date=$1
 id=$2
 audiofile=audio/${date}.flac
+if [ ! -e $audiofile ]
+then
+    echo "audiofile not found"
+    exit
+fi
+
 
 echo "Downloading transcript:"
 curl -X GET   https://api-dot-spraklabben.appspot.com/transcripts/${id}/paragraphs   -H 'Content-Type: application/json'   -H 'Postman-Token: 2ae3e85b-519a-42ce-b42a-dc0dcd8e223f'   -H 'cache-control: no-cache' --user Ohi4V7ctCCfXicc6XjHQ1T1PFDq1:c3cccf8d276d9f4736f7e48961eec1a401b2883c8ee214dcd499a45c4af05407 > intermediary/GET_${date}_$(/bin/date +%Y-%m-%d).json
 
 transcription=intermediary/GET_${date}_$(/bin/date +%Y-%m-%d).json
-copy $transcription non-aligned
+if [ -e $transcription ]
+then
+    cp $transcription non-aligned
+else
+    echo "Transcription not found"
+    exit
+fi
 
 echo "Finding head and tail of audio file"
-head_tail=$(python get_head_tail.py tmp/${date}.flac $transcription)
+head_tail=$(python get_head_tail.py $audiofile $transcription)
 head=$(echo $head_tail | grep  -oP "^\d*\.\d*")
 tail=$(echo $head_tail | grep  -oP "\d*\.\d*$")
+echo "head: $head; tail: $tail"
 
 echo "Converting transcription to lines"
 python json_to_lines.py $transcription
@@ -33,4 +46,4 @@ python align.py $transcription $aeneas aligned/ALIGNED__${date}_$(/bin/date +%Y-
 new_transcription=aligned/ALIGNED__${date}_$(/bin/date +%Y-%m-%d).json
 
 echo "Pushing aligned transcription to Språklabben"
-curl -X PUT   https://api-dot-spraklabben.appspot.com/transcripts/${id}/paragraphs   -H 'Content-Type: application/json'   -H 'Postman-Token: 5e8937eb-9f2d-4af4-8d53-9ccc073fb8a2'   -H 'cache-control: no-cache' --user Ohi4V7ctCCfXicc6XjHQ1T1PFDq1:c3cccf8d276d9f4736f7e48961eec1a401b2883c8ee214dcd499a45c4af05407   --data-binary "@${new_transcription}"
+curl -X PUT   https://api-dot-spraklabben.appspot.com/transcripts/${id}/paragraphs   -H 'Content-Type: application/json'   -H 'Postman-Token: 5e8937eb-9f2d-4af4-8d53-9ccc073fb8a2'   -H 'cache-control: no-cache' --user Ohi4V7ctCCfXicc6XjHQ1T1PFDq1:c3cccf8d276d9f4736f7e48961eec1a401b2883c8ee214dcd499a45c4af05407   --data-binary "@${new_transcription}" > curl_output/PUT__${date}_$(/bin/date +%Y-%m-%d).json
